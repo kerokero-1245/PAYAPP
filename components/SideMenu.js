cb8f399558b1f05eb89856e4c074cb2258b946f8
宇居望 <uinozomu@ukyomochinoMacBook-Air.local>
2025-12-19 23:21:55 +0900

    コンポーネントコメント追加

--- components/SideMenu.js
+++ components/SideMenu.js
@@ -1,5 +1,18 @@
 import Link from "next/link";
 
+/**
+ * 製品一覧画面用のサイドメニューコンポーネント
+ *
+ * @description
+ * 商品一覧ページに表示されるカテゴリナビゲーション。
+ * 現在はリンクをべた書きしているため、製品一覧画面専用の実装。
+ *
+ * @todo
+ * - 表示するメニューの出し分け
+ * - カテゴリを動的に生成する処理の追加
+ *
+ * @returns {JSX.Element} サイドメニューUI
+ */
 export default function SideMenu() {
   return (
     <aside className="w-56 bg-white border-r p-4 sticky top-0 h-screen">
